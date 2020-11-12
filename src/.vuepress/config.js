b491119be9d0630449532aa69b98dda44ef07ce2
Woomy <woomy@woomy.be>
2020-11-12 12:27:50 +0100

    CI

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
+  base: '/blog',
   theme: 'yuu',
   themeConfig: {
     repo: 'Woomy4680-exe',
